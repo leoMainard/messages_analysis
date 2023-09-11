5c607d980741a61532325b64949cfdcaf8dca3f1
Léo Mainard <leomainard63@gmail.com>
2023-09-11 12:21:20 +0200

    api twitter -impossible à réaliser.

--- leo_codes/twitter_api.py
+++ leo_codes/twitter_api.py
@@ -13,8 +13,27 @@ access_token = config['twitter']['access_token']
 access_token_secret = config['twitter']['access_token_secret']
 
 
-
 # Authentification
 auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+
+
+# Récupération de l'id de quelqu'un à partir de son pseudonyme
+print(" --- Récupération des informations de quelqu'un à partir de son @.")
+screen_name = "MiaandeOel"
+arobase = api.get_user(screen_name=screen_name)
+print("@                   :",screen_name)
+print("User id             :", arobase.id)
+print("User name           :", arobase.name)
+print("Description         :", arobase.description)
+print("Compte créé le      :", arobase.created_at.strftime("%d/%m/%Y"))
+print("Nombre de followers :", arobase.followers_count, "\n")
+
+
+
+# Récupération des tweets de la timeline
+tweets = api.user_timeline(screen_name=screen_name)
+tweets
+
+# Impossible à réaliser. Demande une augmentation de version, moyennant paiement.
